e5e925143336866a425a6e946323541b6cfefe63
Anderson Freitas <anderson.freitass@gmail.com>
2017-02-09 18:18:12 -0200

    Added profiles.clj config file.

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[cider/cider-nrepl "0.14.0"]
+                  [com.jakemccrary/lein-test-refresh "0.18.1"]
+                  [lein-ancient "0.6.10"]
+                  [lein-pprint "1.1.2"]
+                  [lein-expectations "0.0.8"]]}
+ :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-alpha14"]]}}

da7e941139d6cbd88c382150bdb241670ec1ef4d
Anderson Freitas <anderson.freitass@gmail.com>
2017-02-23 15:05:39 -0300

    Added JDBC dependency to :repl.

--- profiles.clj
+++ profiles.clj
@@ -3,4 +3,6 @@
                   [lein-ancient "0.6.10"]
                   [lein-pprint "1.1.2"]
                   [lein-expectations "0.0.8"]]}
- :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-alpha14"]]}}
+ :repl {:dependencies [[org.clojure/java.jdbc "0.7.0-alpha1"]
+                       [com.oracle/ojdbc "12.1.0.2"]
+                       ^:displace [org.clojure/clojure "1.9.0-alpha14"]]}}
